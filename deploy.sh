2e075824c4c6489e8b038ce1ed8a425db3391ac9
ndongchrist <christianhonore2003@gmail.com>
2025-09-07 15:37:05 +0100

    fix: Added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Variables
+APP_DIR="/var/www/django-todoapp"
+VENV_DIR="$APP_DIR/venv"
+REQUIREMENTS_FILE="$APP_DIR/requirements.txt"
+
+# Navigate to the application directory
+cd $APP_DIR
+
+# Activate the virtual environment
+source $VENV_DIR/bin/activate
+
+# Install dependencies
+pip install -r $REQUIREMENTS_FILE
+
+# Run database migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Restart the application (e.g., using systemctl)
+python manage.py runserver 0.0.0:8000
