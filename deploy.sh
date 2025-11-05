9af18d63fca213a96af441c32af93b566a497043
ndongchrist <christianhonore2003@gmail.com>
2025-11-05 07:30:47 +0100

    fix: Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Variables
-APP_DIR="/var/www/django-todoapp"
+# APP_DIR="/var/www/django-todoapp"
 
-# Navigate to the application directory
-cd $APP_DIR
+# # Navigate to the application directory
+# cd $APP_DIR
 
-# Pull the latest changes from the repository
-git pull origin main
+# # Pull the latest changes from the repository
+# git pull origin main
 
-chmod +x run.sh
+# chmod +x run.sh
 
 # Stop and remove existing containers
 docker compose down
